7ceee0157e5c62e25bbdfd35ee94891d403b9afe
Rokormen <rokormen@gmail.com>
2020-05-27 15:03:56 +0200

    Modified sql header

--- README.md
+++
@@ -1,3 +0,0 @@
-# 4th-sem
-
-This is a repository for the 4th semester IT subject
--- head/sql_header.php
+++ head/sql_header.php
@@ -1,8 +1,8 @@
 <?php
 
 $dbServername = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
+    $dbUsername = "guest";
+    $dbPassword = "Letm3in!";
     $dbName = "lab6";
 
     $conn = mysqli_connect( $dbServername , $dbUsername , $dbPassword , $dbName );
@@ -10,6 +10,4 @@ $dbServername = "localhost";
     if(!$conn){
         die('Ошибка подключения (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
     }
-
-
 ?>
