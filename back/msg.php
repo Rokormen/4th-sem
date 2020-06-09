6fbeba9fce326e859da3a49a16cca5419b05d0db
Rokormen <rokormen@gmail.com>
2020-06-09 22:31:37 +0300

    added message function for better documentation

--- back/msg.php
+++ back/msg.php
@@ -7,44 +7,49 @@
  */
     include_once "../head/sql_header.php";
 
-    $room_id = $_POST['room'];
-    $token = $_POST['token'];
-    $msg = $_POST['msg'];
+    function msg($room_id, $token, $msg){
+        $query = "SELECT users.login FROM users INNER JOIN tokens ON tokens.id = users.id WHERE tokens.token='$token'";
+        if(!mysqli_query($conn, $query)){
+            $data = (object) array ("type" => "error", "er" => "db");
+            echo json_encode($data, JSON_UNESCAPED_UNICODE);
+            die;
+        }
 
-    $query = "SELECT users.login FROM users INNER JOIN tokens ON tokens.id = users.id WHERE tokens.token='$token'";
-    if(!mysqli_query($conn, $query)){
-        $data = (object) array ("type" => "error", "er" => "db");
-        echo json_encode($data, JSON_UNESCAPED_UNICODE);
-        die;
+        $login = mysqli_fetch_assoc(mysqli_query($conn, $query));
+        $login = $login['login'];
+
+        $query = "SELECT msg FROM rooms WHERE id= '$room_id'";
+        if(!mysqli_query($conn, $query)){
+            $data = (object) array ("type" => "error", "er" => "db");
+            echo json_encode($data, JSON_UNESCAPED_UNICODE);
+            die;
+        }
+
+        $msgm = mysqli_fetch_assoc(mysqli_query($conn, $query));
+        $msgm = json_decode($msgm['msg']);
+
+        $msg = (object) array ("login" => $login, "msg" => $msg);
+    
+        array_push($msgm, $msg);
+
+        $msgm = json_encode($msgm, JSON_UNESCAPED_UNICODE);
+    
+        $query = "UPDATE rooms SET msg='$msgm' WHERE id='$room_id'";
+        if(!mysqli_query($conn, $query)){
+            $data = (object) array ("type" => "error", "er" => "db");
+            echo json_encode($data, JSON_UNESCAPED_UNICODE);
+            die;
+        } else {
+            $data = (object) array ("type" => "success", "tab" => $msgm);
+            echo json_encode($data, JSON_UNESCAPED_UNICODE);
+            die;
+        }
     }
 
-    $login = mysqli_fetch_assoc(mysqli_query($conn, $query));
-    $login = $login['login'];
-
-    $query = "SELECT msg FROM rooms WHERE id= '$room_id'";
-    if(!mysqli_query($conn, $query)){
-        $data = (object) array ("type" => "error", "er" => "db");
-        echo json_encode($data, JSON_UNESCAPED_UNICODE);
-        die;
-    }
-
-    $msgm = mysqli_fetch_assoc(mysqli_query($conn, $query));
-    $msgm = json_decode($msgm['msg']);
-
-    $msg = (object) array ("login" => $login, "msg" => $msg);
-    
-    array_push($msgm, $msg);
-
-    $msgm = json_encode($msgm, JSON_UNESCAPED_UNICODE);
-    
-    $query = "UPDATE rooms SET msg='$msgm' WHERE id='$room_id'";
-    if(!mysqli_query($conn, $query)){
-        $data = (object) array ("type" => "error", "er" => "db");
-        echo json_encode($data, JSON_UNESCAPED_UNICODE);
-        die;
-    } else {
-        $data = (object) array ("type" => "success", "tab" => $msgm);
-        echo json_encode($data, JSON_UNESCAPED_UNICODE);
-        die;
+    if(isset($_POST['msg'])){ 
+        $room_id = $_POST['room'];
+        $token = $_POST['token'];
+        $msg = $_POST['msg'];
+        msg($room_id, $token, $msg);
     }
 ?>
